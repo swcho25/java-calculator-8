2bf9ef2121a1162e963fcb5357da61cd1d18e558
Seokwon Cho <dkf1141@gmail.com>
2025-10-16 14:58:53 +0900

    feat(view): Console API를 통한 문자열 입력 기능 추가

---
+++ src/main/java/calculator/Calculator.java
@@ -0,0 +1,11 @@
+package calculator;
+
+import camp.nextstep.edu.missionutils.Console;
+
+public class Calculator {
+    public void run(){
+        System.out.println("덧셈할 문자열을 입력해 주세요.");
+        String input = Console.readLine();
+        System.out.println("입력값: " + input);
+    }
+}
