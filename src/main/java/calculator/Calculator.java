1a47254db5d52da675b613287ff9b063cbd18038
Seokwon Cho <dkf1141@gmail.com>
2025-10-17 01:33:28 +0900

    feat(service): 구분자를 통한 문자 분리
    - 일반 구분자를 통한 문자 분리
    
    - 커스텀 구분자를 통한 문자 분리
    
    - 일반+커스텀 구분자 동시 사용 가능

--- src/main/java/calculator/Calculator.java
+++ src/main/java/calculator/Calculator.java
@@ -1,11 +1,34 @@
 package calculator;
 
 import camp.nextstep.edu.missionutils.Console;
+import java.util.Arrays;
+import java.util.regex.Pattern;
 
 public class Calculator {
     public void run(){
         System.out.println("덧셈할 문자열을 입력해 주세요.");
         String input = Console.readLine();
-        System.out.println("입력값: " + input);
+        String[] inputs = splitInput(input);
+        System.out.println("입력된 숫자 값: " + Arrays.toString(inputs));
+    }
+
+    private String[] splitInput(String input) {
+        String customDelimiter = "";
+        String inputs = input;
+
+        if (input.startsWith("//")) {
+            // "\n"만 사용하면 엔터값으로 인식하기 때문에 "\n"을 문자 자체로 인식하기 위해 "\\n" 사용
+            int delimiterIndex = input.indexOf("\\n");
+            // quote() -> .이나 ;와 같은 예약어와 같이 특별한 의미를 갖는 문자를 일반 문자로 인식하게 함
+            customDelimiter = Pattern.quote(input.substring(2, delimiterIndex));
+            inputs = input.substring(delimiterIndex + 2);
+        }
+
+        String delimiter = ",|:";
+        if (!customDelimiter.isEmpty()) {
+            delimiter += "|" + customDelimiter;
+        }
+
+        return inputs.split(delimiter);
     }
 }
