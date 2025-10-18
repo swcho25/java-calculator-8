7115769e1473de1d8fb3e339cbeb3ab444eee955
Seokwon Cho <dkf1141@gmail.com>
2025-10-18 13:57:03 +0900

    feat(service): 입력 관련 예외 처리 기능 추가
    - 아무 값도 입력되지 않은 경우 처리
    
    - 구분자가 존재하지 않는 경우 처리
    
    - 구분자만 입력된 경우 처리
    
    - 구분자가 비정상적으로 입력된 경우 처리

--- src/main/java/calculator/Calculator.java
+++ src/main/java/calculator/Calculator.java
@@ -8,6 +8,9 @@ public class Calculator {
     public void run(){
         System.out.println("덧셈할 문자열을 입력해 주세요.");
         String input = Console.readLine();
+
+        checkInput(input);
+
         String[] inputs = splitInput(input);
         int result = calculateInput(inputs);
         System.out.println("결과: " + result);
@@ -39,5 +42,28 @@ public class Calculator {
             sum += Integer.parseInt(input);
         }
         return sum;
+    }
+
+    private void checkInput(String input) {
+        // 입력 요청 시 값이 입력되지 않은 경우
+        if(input==null) {
+            throw new IllegalArgumentException("입력 값이 비어있습니다.");
+        }
+        // 입력된 문자열에 구분자가 존재하지 않는 경우
+        boolean hasCommon = input.contains(",")||input.contains(":");
+        boolean hasCustom = input.startsWith("//");
+        if(!hasCommon && !hasCustom) {
+            throw new IllegalArgumentException("구분자가 존재하지 않습니다.");
+        }
+        // 입력된 문자열에 구분자만 작성된 경우
+        if(input.matches("^[^0-9]+$")) {
+            throw new IllegalArgumentException("숫자가 입력되지 않았습니다.");
+        }
+        // 입력된 문자열에 구분자가 비정상적으로 입력된 경우
+        if(hasCustom) {
+            if(!input.contains("\n")){
+                throw new IllegalArgumentException("커스텀 구분자 선언 형식이 잘못되었습니다.");
+            }
+        }
     }
 }
